e8bed6d8b70483c73249873cf01ce8141fb00228
Andhika Hazra <andhika.5230411095@student.uty.ac.id>
2026-07-15 12:44:26 +0700

    feat: add custom framework pagination view templates to resources folder

---
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,71 @@
+@if ($paginator->hasPages())
+    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="pagination flex-wrap">
+
+        {{-- Info: Showing X to Y of Z --}}
+        <div class="flex items-center gap-2">
+            <p class="text-sm text-slate-500 dark:text-zinc-400 mr-1">
+                {!! __('Showing') !!}
+                @if ($paginator->firstItem())
+                    <span class="font-medium">{{ $paginator->firstItem() }}</span>
+                    {!! __('to') !!}
+                    <span class="font-medium">{{ $paginator->lastItem() }}</span>
+                @else
+                    {{ $paginator->count() }}
+                @endif
+                {!! __('of') !!}
+                <span class="font-medium">{{ $paginator->total() }}</span>
+                {!! __('results') !!}
+            </p>
+        </div>
+
+        {{-- Page Buttons --}}
+        <div class="flex items-center gap-2">
+            {{-- Previous --}}
+            @if ($paginator->onFirstPage())
+                <span class="page-btn cursor-not-allowed opacity-50" aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
+                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+            @else
+                <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="page-btn" aria-label="{{ __('pagination.previous') }}">
+                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                    </svg>
+                </a>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                @if (is_string($element))
+                    <span class="page-btn cursor-default opacity-60">{{ $element }}</span>
+                @endif
+
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <span aria-current="page" class="page-btn active">{{ $page }}</span>
+                        @else
+                            <a href="{{ $url }}" class="page-btn" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">{{ $page }}</a>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next --}}
+            @if ($paginator->hasMorePages())
+                <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="page-btn" aria-label="{{ __('pagination.next') }}">
+                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                    </svg>
+                </a>
+            @else
+                <span class="page-btn cursor-not-allowed opacity-50" aria-disabled="true" aria-label="{{ __('pagination.next') }}">
+                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+            @endif
+        </div>
+    </nav>
+@endif
