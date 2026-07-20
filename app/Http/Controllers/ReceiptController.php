d1086332258891f7fb2af29ab78776bbbb88d674
Andhika Hazra <andhika.5230411095@student.uty.ac.id>
2026-07-20 14:07:01 +0700

    feat: implement dynamic digital receipt view with auto-generated OG images for social sharing

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -38,8 +38,8 @@ class ReceiptController extends Controller
         $storeName    = $setting->nama_toko ?? 'MitraPOS';
         $ogTitle      = "Nota {$transaksi->kode} | {$storeName}";
         $ogDescription = "Total: Rp " . number_format($grandTotal, 0, ',', '.') . " | {$method}";
-        $ogImage      = route('nota.og-image', $transaksi->kode);
-        $ogUrl        = route('nota.show', $transaksi->kode);
+        $ogImage      = config('app.url') . '/nota/' . $transaksi->kode . '/og-image';
+        $ogUrl        = config('app.url') . '/nota/' . $transaksi->kode;
 
         $totals = [
             'subtotal'    => $subtotal,
@@ -70,62 +70,62 @@ class ReceiptController extends Controller
 
         $width  = 600;
         $height = 315;
-        $img    = imagecreatetruecolor($width, $height);
+        $img    = \imagecreatetruecolor($width, $height);
 
-        $bg         = imagecolorallocate($img, 255, 255, 255);
-        $dark       = imagecolorallocate($img, 15, 23, 42);
-        $slate      = imagecolorallocate($img, 100, 116, 139);
-        $emerald    = imagecolorallocate($img, 16, 185, 129);
-        $border     = imagecolorallocate($img, 226, 232, 240);
+        $bg         = \imagecolorallocate($img, 255, 255, 255);
+        $dark       = \imagecolorallocate($img, 15, 23, 42);
+        $slate      = \imagecolorallocate($img, 100, 116, 139);
+        $emerald    = \imagecolorallocate($img, 16, 185, 129);
+        $border     = \imagecolorallocate($img, 226, 232, 240);
 
-        imagefill($img, 0, 0, $bg);
+        \imagefill($img, 0, 0, $bg);
 
-        imagerectangle($img, 0, 0, $width - 1, $height - 1, $border);
+        \imagerectangle($img, 0, 0, $width - 1, $height - 1, $border);
 
-        imagefilledrectangle($img, 1, 1, $width - 2, 56, $dark);
+        \imagefilledrectangle($img, 1, 1, $width - 2, 56, $dark);
 
         $font = 5;
-        $textW = imagefontwidth($font) * strlen($storeName);
-        imagestring($img, $font, (int) (($width - $textW) / 2), 20, $storeName, $bg);
+        $textW = \imagefontwidth($font) * strlen($storeName);
+        \imagestring($img, $font, (int) (($width - $textW) / 2), 20, $storeName, $bg);
 
         $date = $transaksi->tanggal?->format('d M Y H:i') ?? '-';
         $infoY = 75;
 
-        imagestring($img, 4, 30, $infoY, "No: {$transaksi->kode}", $slate);
-        imagestring($img, 4, 30, $infoY + 22, "Tgl: {$date}", $slate);
-        imagestring($img, 4, 30, $infoY + 44, "Byr: {$method}", $slate);
+        \imagestring($img, 4, 30, $infoY, "No: {$transaksi->kode}", $slate);
+        \imagestring($img, 4, 30, $infoY + 22, "Tgl: {$date}", $slate);
+        \imagestring($img, 4, 30, $infoY + 44, "Byr: {$method}", $slate);
 
         $rightX = $width - 30;
         $totalLabel = "TOTAL";
-        $totalLabelW = imagefontwidth(4) * strlen($totalLabel);
-        imagestring($img, 4, $rightX - $totalLabelW - 80, $infoY, $totalLabel, $slate);
+        $totalLabelW = \imagefontwidth(4) * strlen($totalLabel);
+        \imagestring($img, 4, $rightX - $totalLabelW - 80, $infoY, $totalLabel, $slate);
 
         $totalText = "Rp " . number_format($grandTotal, 0, ',', '.');
-        $totalTextW = imagefontwidth(5) * strlen($totalText);
-        imagestring($img, 5, $rightX - $totalTextW, $infoY + 22, $totalText, $emerald);
+        $totalTextW = \imagefontwidth(5) * strlen($totalText);
+        \imagestring($img, 5, $rightX - $totalTextW, $infoY + 22, $totalText, $emerald);
 
         $itemsY = $infoY + 80;
-        imagerectangle($img, 30, $itemsY, $width - 30, $height - 30, $border);
+        \imagerectangle($img, 30, $itemsY, $width - 30, $height - 30, $border);
         $y = $itemsY + 10;
         $count = 0;
         foreach ($transaksi->detail_transaksi as $detail) {
             if ($count >= 4) {
-                imagestring($img, 4, 40, $y, "...", $slate);
+                \imagestring($img, 4, 40, $y, "...", $slate);
                 break;
             }
             $name = $detail->produk->nama ?? '-';
-            imagestring($img, 4, 40, $y, $name, $dark);
+            \imagestring($img, 4, 40, $y, $name, $dark);
             $sub = "Rp " . number_format($detail->subtotal, 0, ',', '.');
-            $subW = imagefontwidth(4) * strlen($sub);
-            imagestring($img, 4, $rightX - $subW - 10, $y, $sub, $slate);
+            $subW = \imagefontwidth(4) * strlen($sub);
+            \imagestring($img, 4, $rightX - $subW - 10, $y, $sub, $slate);
             $y += 22;
             $count++;
         }
 
         ob_start();
-        imagepng($img);
+        \imagepng($img);
         $imageData = ob_get_clean();
-        imagedestroy($img);
+        \imagedestroy($img);
 
         return response($imageData, 200, [
             'Content-Type' => 'image/png',
